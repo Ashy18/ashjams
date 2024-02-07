9317989aaecd59d87db4cc0561e6a1bec785eff6
Ashy18 <116997842+Ashy18@users.noreply.github.com>
2024-02-08 00:02:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const titleElement = document.querySelector(".title");
 const buttonsContainer = document.querySelector(".buttons");
 const yesButton = document.querySelector(".btn--yes");
 const noButton = document.querySelector(".btn--no");
-const catImg = document.querySelector(".kuromi-img");
+const catImg = document.querySelector(".kuromi-gif");
 
 const MAX_IMAGES = 5;
 
